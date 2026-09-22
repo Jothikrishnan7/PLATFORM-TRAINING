ab742ab30ccee56a0067df6ba28bbdc4ac18f1b1
Jothikrishnan7 <jfuthram2006@gmail.com>
2026-09-22 14:57:42 +0530

    Create pyq3.py

---
+++ pyq3.py
@@ -0,0 +1,24 @@
+j=int(input("enter the first number"))
+k=int(input("enter the second number"))
+print("Using kin")
+kin=j
+j=k
+k=kin
+print("j=",j)
+print("k=",k)
+print("Using ,")
+j,k=k,j
+print("j =", j)
+print("k =", k)
+print("Using + and -")
+j=j+k
+k=j-k
+j=j-k
+print("j =", j)
+print("k =", k
+print("Using ^")
+j=j^k
+k=j^k
+j=j^k
+print("j =", j)
+print("k =", k)
